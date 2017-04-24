28cd7cfd2050638be8411479cd3765cbf23da268
Chrisjs28 <christopher.curran@sky.uk>
2017-04-24 07:57:51 +0100

    Redsign half way

---
+++ app/src/main/java/com/example/android/cloudy/adpaters/ForecastAdapter.java
@@ -0,0 +1,8 @@
+package com.example.android.cloudy.adpaters;
+
+/**
+ * Created by ccu17 on 24/04/2017.
+ */
+
+public class ForecastAdapter {
+}
---
+++ app/src/main/java/com/example/android/cloudy/data/model/remote/CollectFiveDayForecast.java
@@ -0,0 +1,8 @@
+package com.example.android.cloudy.data.model.remote;
+
+/**
+ * Created by ccu17 on 24/04/2017.
+ */
+
+public class CollectFiveDayForecast {
+}
