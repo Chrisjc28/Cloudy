b92235a63332f7a9a1b2e55f18d3db4d89fd0538
Chrisjs28 <christopher.curran@sky.uk>
2017-04-26 09:10:55 +0100

    addedd voew pager adpater & fragments

---
+++ app/src/main/java/com/example/android/cloudy/fragments/CurrentForecastFragment.java
@@ -0,0 +1,32 @@
+package com.example.android.cloudy.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.cloudy.R;
+
+/**
+ * Created by ccu17 on 26/04/2017.
+ */
+
+public class CurrentForecastFragment extends Fragment {
+
+    public CurrentForecastFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_current_forecast, container, false);
+    }
+}
